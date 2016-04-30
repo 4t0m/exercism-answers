62af185cf89fc66e8d5de26c0badf441c6403d3a
4t0m <adomhartell@gmail.com>
2016-04-29 22:41:14 -0500

    Update food_chain.rb

--- ruby/food-chain/food_chain.rb
+++ ruby/food-chain/food_chain.rb
@@ -1,32 +1,27 @@
 class FoodChain
   VERSION = 2
   def self.song
-    animals = ["fly", "spider", "bird", "cat", "dog", "goat", "cow", "horse"]
+    animals = %w(fly spider bird cat dog goat cow horse)
     unique_strings = {
-      "fly" => "I don't know why she swallowed the fly. Perhaps she'll die.\n",
-      "spider" => "It wriggled and jiggled and tickled inside her.",
-      "bird" => "How absurd to swallow a bird!",
-      "cat" => "Imagine that, to swallow a cat!",
-      "dog" => "What a hog, to swallow a dog!",
-      "goat" => "Just opened her throat and swallowed a goat!",
-      "cow" => "I don't know how she swallowed a cow!",
-      "horse" =>  "She's dead, of course!"
+      fly: "I don't know why she swallowed the fly. Perhaps she'll die.\n",
+      spider: "It wriggled and jiggled and tickled inside her.",
+      bird: "How absurd to swallow a bird!",
+      cat: "Imagine that, to swallow a cat!",
+      dog: "What a hog, to swallow a dog!",
+      goat: "Just opened her throat and swallowed a goat!",
+      cow: "I don't know how she swallowed a cow!",
+      horse: "She's dead, of course!"
     }
 
-    full_text = ""
-    accum_text = "#{unique_strings["fly"]}\n"
-    animals.each_with_index do |animal,index|
+    accum_text = "#{unique_strings[:fly]}\n"
+    animals.each_with_index.with_object("") do |(animal , index) , full_text|
 
-      flavor_text = "#{unique_strings[animal]}\n"
+      flavor_text = "#{unique_strings[animal.to_sym]}\n"
       i_know = "I know an old lady who swallowed a #{animal}.\n#{flavor_text}"
 
-      if index == 2
-        last_animal = animals[index-1]
-        new_text = "She swallowed the #{animal} to catch the #{last_animal} that wriggled and jiggled and tickled inside her.\n"
-      elsif index > 0
-        last_animal = animals[index-1]
-        new_text = "She swallowed the #{animal} to catch the #{last_animal}.\n"
-      end
+      prev_animal = animals[index - 1]
+      ending = index == 2 ? " that #{unique_strings[:spider][3..-1]}\n" : ".\n"
+      new_text = "She swallowed the #{animal} to catch the #{prev_animal}#{ending}"
 
       full_text << "#{i_know}"
       unless animal == "horse" || animal == "fly"
@@ -34,8 +29,5 @@ class FoodChain
         full_text << accum_text
       end
     end
-    full_text
   end
 end
-
-puts FoodChain.song
