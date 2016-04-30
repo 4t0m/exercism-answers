24b01ab9d42af12089c010727db984c68aa0f1d8
4t0m <adomhartell@gmail.com>
2016-04-30 04:17:14 -0500

    Update run_length_encoding.rb

--- ruby/run-length-encoding/run_length_encoding.rb
+++ ruby/run-length-encoding/run_length_encoding.rb
@@ -2,51 +2,14 @@ class RunLengthEncoding
   VERSION = 1
 
   def self.encode(plain_text)
-    encoded_string = ""
-    runs = self.tally(plain_text)
-    runs.each do |run|
-      value = run[0]
-      run_length = run.count.to_s
-      if run.count > 1
-        encoded_string << "#{run_length}#{value}"
-      else
-        encoded_string << value
-      end
+    plain_text.gsub(/(.)\1*/) do |run|
+      run_length = run.length == 1 ? "" : run.length
+      "#{run_length}#{run[0]}"
     end
-    encoded_string
   end
 
   def self.decode(coded_text)
-    run_length = ""
-    plain_text = ""
-    numbers = "1234567890".split("")
-    coded_text.each_char do |char|
-      if numbers.include?(char)
-        run_length << char
-      elsif run_length == ""
-        plain_text << char
-      else
-        run_length.to_i.times{plain_text << char}
-        run_length = ""
-      end
-    end
-    plain_text
-  end
-
-  def self.tally(plain_text)
-    current_char = plain_text[0]
-    array = [[]]
-    index = 0
-
-    plain_text.each_char do |char|
-      if char == current_char
-        array[index] << char
-      else
-        index += 1
-        array[index] = [char]
-        current_char = char
-      end
-    end
-  array
+    coded_text.gsub(/(\d+)(\D)/){$2 * $1.to_i}
   end
 end
+
