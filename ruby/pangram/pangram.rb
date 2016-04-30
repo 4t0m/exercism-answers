3985386ff9875613913f2a2a6bf9fff39dbeefc6
4t0m <adomhartell@gmail.com>
2016-04-29 20:29:43 -0500

    Update pangram.rb

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,17 +1,8 @@
 class Pangram
   VERSION = 1
-
   def self.is_pangram?(string)
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
-
-    all_letters = string.downcase.chars
-    #puts all_letters.inspect
-    alphabet.each_char do |char|
-      #puts "this fails bc #{char}" unless all_letters.include?(char)
-      return false unless all_letters.include?(char)
+    ("a".."z").each do |letter|
+      return false unless string.downcase.include?(letter)
     end
-    true
   end
 end
-
-puts Pangram.is_pangram?('"Five quacking Zephyrs jolt my wax bed."')
