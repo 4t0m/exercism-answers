2ffc382db7d83e70483f95e7e59c6311782bba18
4t0m <adomhartell@gmail.com>
2016-04-29 20:19:09 -0500

    Update hamming.rb

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -2,10 +2,7 @@ class Hamming
   VERSION = 1
   def self.compute(strand_1, strand_2)
     raise ArgumentError.new() unless strand_1.length == strand_2.length
-    count = 0
-    strand_1.chars.each_with_index do |code, index|
-      count += 1 if code != strand_2[index]
-    end
-    count
+    (0...strand_1.length).inject(0) do |diff_count, index|
+      strand_1[index] == strand_2[index] ? diff_count : diff_count += 1
   end
 end
