c10021f643917267abb2c1d0db005ef4771c8a2c
4t0m <adomhartell@gmail.com>
2016-04-29 23:08:55 -0500

    Update hamming.rb

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,8 +1,9 @@
 class Hamming
   VERSION = 1
   def self.compute(strand_1, strand_2)
-    raise ArgumentError.new() unless strand_1.length == strand_2.length
-    (0...strand_1.length).inject(0) do |diff_count, index|
-      strand_1[index] == strand_2[index] ? diff_count : diff_count += 1
+    if strand_1.length != strand_2.length
+      raise ArgumentError.new, "Strands are of different length."
+    end
+    (0...strand_2.length).count{|index|strand_1[index] != strand_2[index]}
   end
 end
