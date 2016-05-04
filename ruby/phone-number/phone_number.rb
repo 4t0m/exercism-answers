6da6635962abdd94484d3b06c983d8fcb7a796af
4t0m <adomhartell@gmail.com>
2016-05-03 22:22:31 -0500

    Update phone_number.rb

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -3,10 +3,10 @@ class PhoneNumber
   attr_reader :area_code
 
   def initialize(input)
-    @number , @area_code = PhoneNumber.interpret_number(input)
+    @number , @area_code = interpret_number(input)
   end
 
-  def self.interpret_number(input)
+  def interpret_number(input)
     number = input.match /(1)?\D*(\d{3})\D*(\d{3})\D*(\d{4})(\d)?/
     if number && number[5].nil?
       [number[2] + number[3] + number[4], number[2]]
@@ -14,9 +14,11 @@ class PhoneNumber
       ["0000000000", "000"]
     end
   end
+  private :interpret_number
+
 
   def to_s
     @number.gsub(/(\d{3})(\d{3})(\d{4})/){"(#{$1}) #{$2}-#{$3}"}
   end
-  
+
 end
